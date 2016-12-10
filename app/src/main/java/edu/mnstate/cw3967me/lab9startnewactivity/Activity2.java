5a53892f1b8ef885f2f4b03465a543e4d9641081
野口　茉莉子 <marikonoguchi@noguchimariko-no-MacBook-Air.local>
2016-12-10 12:56:00 -0600

    Initial commit

---
+++ app/src/main/java/edu/mnstate/cw3967me/lab9startnewactivity/Activity2.java
@@ -0,0 +1,13 @@
+package edu.mnstate.cw3967me.lab9startnewactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
